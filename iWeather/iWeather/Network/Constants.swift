c5901c3abd565a3b02038d4e3ecf5eae94a09a1b
swelenebert <swelen@ciandt.com>
2022-08-23 16:21:53 -0300

    network

---
+++ iWeather/iWeather/Network/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  iWeather
+//
+//  Created by Swelen Poliana Cesario Ebert on 23/08/22.
+//
+
+import Foundation
+
+public class Constants {
+
+    static let API_KEY = "938a1f16302c36b04935b36ded625acd"
+    static let host = "api.openweathermap.org"
+    static let path = "/data/2.5/weather"
+    static let forecastPath = "/data/2.5/forecast"
+    static let units = "metric"
+    
+    static let iconHost = "https://openweathermap.org/img/wn//2x.png"
+    static let iconPath = "/img/wn"
+    static let iconExtension = "/2x.png"
+}
